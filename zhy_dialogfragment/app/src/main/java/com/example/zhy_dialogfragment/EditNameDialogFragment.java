63a18127b9f67804363a8aa441488e93bfcfcda6
jspfei <jiangfeifan8@163.com>
2017-01-03 18:21:07 +0800

    sss

---
+++ zhy_dialogfragment/app/src/main/java/com/example/zhy_dialogfragment/EditNameDialogFragment.java
@@ -0,0 +1,26 @@
+package com.example.zhy_dialogfragment;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.Window;
+
+public class EditNameDialogFragment extends DialogFragment
+{
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState)
+	{
+		if (getResources().getBoolean(R.bool.large_layout))
+		{
+			getDialog().requestWindowFeature(Window.FEATURE_NO_TITLE);
+		}
+		View view = inflater.inflate(R.layout.fragment_edit_name, container,
+				false);
+		return view;
+	}
+
+}
